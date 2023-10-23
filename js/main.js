219b73d1586d473dbf5bf1ca280a4abe70df4390
Esteph <estephvr@gmail.com>
2023-10-23 18:50:06 -0500

    Se agrega ejercicio de hoy, lista de compras

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 // El código va aquí -> 
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
+let btnAgregar = document.getElementById("btnAgregar");
+//Se validan cuando de click en los botones: Agregar
+let alertValidaciones = document.getElementById("alertValidaciones");
+
+function validarCantidad(){
+    if (txtNumber.value.length==0){
+        return false;
+    }//if length
+    return true;
+}//validarCantidad function
+
+btnAgregar.addEventListener("click", function(event){
+    event.preventDefault(); //para que no haga nada si no es necesario?
+    if (txtNombre.value.length<3){
+        alertValidaciones.innerHTML="El campo <strong>Nombre</strong> es requerido <br/>"
+        alertValidaciones.style.display="block"; //Lo muestra en Bloque
+    }//If txtNombre <3
+
+    if (! validarCantidad()){
+        alertValidaciones.innerHTML+="El campo <strong>Cantidad</strong> es requerido"
+        alertValidaciones.style.display="block"; //Lo muestra en Bloque
+    }//validarCantidad
+});//EvenListener
